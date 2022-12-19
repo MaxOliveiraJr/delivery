22d137e8a1e9cbb99f07231c82bb51b01e8a3da1
Max Oliveira <max-oliveira@outlook.com.br>
2022-12-18 22:31:04 -0300

    feat: header complete

---
+++ components/SearchInput/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import styles from "./styles.module.css";
+
+type Props = {
+  mainColor: string;
+  onSearch: (searchValue: string) => void;
+};
+
+export const SearchInput = ({ mainColor, onSearch }: Props) => {
+  const [focused, setFocused] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === "Enter") {
+      onSearch(searchValue);
+    }
+  };
+
+  return (
+    <div
+      className={styles.container}
+      style={{ borderColor: focused ? mainColor : "#ffffff" }}
+    >
+      <div
+        className={styles.button}
+        onClick={(e) => onSearch(searchValue)}
+      ></div>
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Digite o nome do que procura"
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onKeyUp={handleKeyUp}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+    </div>
+  );
+};
